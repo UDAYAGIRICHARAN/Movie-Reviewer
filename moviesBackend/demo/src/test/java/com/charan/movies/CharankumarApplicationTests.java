63d4a2676e3fb9f5581a2f6b2742811637a0a79c
CHARAN KUMAR UDAYAGIRI <67089878+UDAYAGIRICHARAN@users.noreply.github.com>
2023-04-01 19:09:14 +0530

    done

---
+++ moviesBackend/demo/src/test/java/com/charan/movies/CharankumarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charan.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharankumarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
